a4d574dea566cdd074fe5636cb8a37f279348290
Rafael García <rafaelgarcia1985@hotmail.com>
2026-07-29 20:54:39 +0200

    chore: release version 0.10.0

--- src/appVersion.ts
+++ src/appVersion.ts
@@ -1,5 +1,5 @@
 const appActivationStorageKey = "jucart:app-release";
-const fallbackVersion = "0.9.0";
+const fallbackVersion = "0.10.0";
 const fallbackBuildDate = "1970-01-01T00:00:00.000Z";
 
 export type AppReleaseInfo = {
